a9cf841f5ab76806d98070fad3a2df4d8f28346e
Christian Polli <74564883+Maracujacake@users.noreply.github.com>
2023-12-29 22:00:41 +0000

    edições comentadas e ajuste na referencia dos arquivos

--- resources/views/ponto.blade.php
+++ resources/views/ponto.blade.php
@@ -3,10 +3,22 @@
 @section('title', 'Ponto')
 
 @section('content')
+<!--
+<link rel="stylesheet" href="{{ asset('css/pontoStyle.css') }}">
+<div class="container">
+    <h2 class="heading">
+        <a href="#" id="pontoButton">Bater Ponto<span class="check-icon"><i class="fas fa-check"></i></span></a>
+    </h2>
+    <p class="user-info" id="userInfo">{{ auth()->user()->name }} | <a href="{{ route('login.destroy') }}" class="logout-link">Log out</a></p>
+    <p class="message">Ponto realizado</p>
+    <div class="second-click-time center-text" id="secondClickTime"></div>
+</div>
+
+<script src="{{ asset('js/ponto.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js"></script>
+-->
 
 <h2>Bater ponto</h2>
 {{ auth()->user()->name }} | <a href="{{ route('login.destroy') }}">Log out</a>
-
-
 
 @endsection
