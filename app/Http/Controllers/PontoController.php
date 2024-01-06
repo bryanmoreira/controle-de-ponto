8b9a89f2e2800c9717204ef5595c36713efb2910
Bryan <bryan@lampadaglobal.com>
2024-01-05 22:30:03 -0300

    Função para salvar os dados do user na hora de bater o ponto

--- app/Http/Controllers/PontoController.php
+++ app/Http/Controllers/PontoController.php
@@ -2,11 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Ponto;
+use Illuminate\Support\Facades\Auth;
 
 class PontoController extends Controller
 {
-    public function index() {
-        return view('ponto');
+    public function index() 
+    {
+        $user = Auth::id();
+        
+        $ultimoPonto = Ponto::where('user_id', $user)
+            ->orderBy('created_at', 'desc')
+            ->value('created_at');
+
+        if($ultimoPonto) {
+            $ultimoPonto = $ultimoPonto->format('H:i:s');
+        }
+        
+        return view('ponto')->with('hora', $ultimoPonto);
+    }
+
+    public function store()
+    {
+        $user = Auth::user();
+
+        Ponto::create([
+            'user_id' => $user->id,
+            'user_name' => $user->name,
+        ]);
+
+        return redirect()->route('ponto.index');
     }
 }
--- resources/views/ponto.blade.php
+++ resources/views/ponto.blade.php
@@ -4,11 +4,23 @@
 
 @section('content')
 
+@if ($hora)
+    <h1>Ponto batido: {{ $hora }}</h1>
+@endif
+
 
 <link rel="stylesheet" href="{{ asset('css/pontoStyle.css') }}">
 <div class="container">
     <h2 class="heading">
-        <a href="#" id="pontoButton">Bater Ponto<span class="check-icon"><i class="fas fa-check"></i></span></a>
+        <form action="{{ route('ponto.store') }}" method="POST">
+            @csrf
+            <button type="submit">
+                Registrar ponto
+                <span class="check-icon">
+                    <i class="fas fa-check"></i>
+                </span>
+            </button>
+        </form>
     </h2>
     <p class="user-info" id="userInfo">{{ auth()->user()->name }} | <a href="{{ route('login.destroy') }}" class="logout-link">Log out</a></p>
     <p class="message">Ponto realizado</p>
